050dbfedf1d2f0f653664bb3d85e28db6b50b645
Alina Mishchenko <153620209+AlinaMishchenk@users.noreply.github.com>
2024-09-09 20:49:24 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,57 +2,59 @@
 
 #The architecture of your operating system and its kernel version
 
-echo 
-uname -o -v
-echo
+uname -ov | awk  {'print "#Architecture: \t" $0'}
 
 #The number of physical processors
 
-grep "physical id" /proc/cpuinfo | sort -u | wc -l
+lscpu | grep '^CPU(s):' | awk '{print "#CPU physical: \t" $2 }'
 
 #The number of virtual processors/logical CPU
 
-grep '^processor' /proc/cpuinfo | wc -l
+lscpu | grep 'Thread(s) per core:' | awk '{ print "#vCPU: \t" $4 }'
 
-#The current avail. RAM and its utilization rate as a percentage
- 
-free | awk '/Mem:/ {printf("RAM Utilization: %.2f%%\n", 100 * ($3 / $2))}'
+#The current avail. RAM and its utilization rate as a \percentage
+
+ free -m | awk 'NR==2{printf "Available RAM: %sMB | Utilization: %.2f%%\n", $7, $3*(100/$2) }'
 
 #The current available storage on your server and its utilization rate as %
 
-df -h | awk '$NF=="/"{printf("Disk Utilization: %.2f%%\n", 100 * ($3 / $2))}'
+df -h / | awk 'NR==2 {printf "Available Storage: %s | Utilization: %s\n", $4, $5}' 
 
-#The current utilization rate of your processors as a percentage?
+#The current utilization rate of your processors as a percentage
 
-grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}' 
+top -bn1 | grep "Cpu(s)" | awk -F" " '{printf "#CPU load: \t"$2 }'
+
 
 #The date and time of the last reboot
 
-who -b
+who -b | awk '{print "System boot:", $3, $4}'
 
-#Whether LVM is active or not ? 
+#Whether LVM is active or not.
 
-if vgs --noheadings | grep -q .; then
-    echo "yes"
+if pvs &> /dev/null; then
+printf "LVM use: yes\n"
 else
-    echo "no"
+printf "LVM use: no\n"
 fi
+
 
 #The number of active connections
 
-ss -tna | grep ESTAB | wc -l
+printf "Connections TCP: %d ESTABLISHED\n" $(ss -tna | grep ESTAB | wc -l)
+
 
 #The number of users using the server.
 
-users | wc -w
+printf "User log: %d\n" $(users | wc -w)
 
 #The IPv4 address of your server and its MAC (Media Access Control) address
-echo "IPv4 Address: $(ip -4 addr show | grep inet | awk '{print $2}' | cut -d/ -f1)"
-echo "MAC Address: $(ip link show | grep ether | awk '{print $2}')"
+echo "IPv4 Address: $(hostname -I | awk '{print $1}') | MAC Address: $(ip link | grep 'link/ether' | awk '{print $2}' | head -n 1)"
+
+
 
 #The number of commands executed with the sudo program
-grep 'sudo:' /var/log/auth.log | wc -l
 
+sudo journalctl _COMM=sudo | wc -l | awk '{print "sudo:", $1}'
 
 
 another
